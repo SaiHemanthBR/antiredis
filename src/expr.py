f284b3fd8e0532f959b22f95fc21c99b3ad3c6bd
SaiHemanthBR <sh.bheemreddy@gmail.com>
2022-01-22 22:08:49 +0530

    Added FuncCall eval

--- src/expr.py
+++ src/expr.py
@@ -10,6 +10,23 @@ class Expression(Token):
     pass
 
 
+class FuncCall(Token):
+    def __init__(self, func_name, args):
+        super().__init__()
+        self.func_name = func_name
+        self.args = args
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} func_name='{self.func_name}' args='{self.args}'>"
+
+    def eval(self, context):
+        return 0
+
+    @classmethod
+    def parse_action(cls, toks):
+        return FuncCall(toks[0], toks[1])
+
+
 class UniaryOp(Token):
     op_map = {
         '+': lambda rhs: +rhs,
@@ -17,7 +34,7 @@ class UniaryOp(Token):
         '~': lambda rhs: ~rhs,
     }
 
-    def __init__(self, rhs, op) -> None:
+    def __init__(self, rhs, op):
         super().__init__()
         self.rhs = rhs
         self.op = op
@@ -25,8 +42,8 @@ class UniaryOp(Token):
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} operand='{self.rhs}' op='{self.op}'>"
 
-    def eval(self):
-        return UniaryOp.op_map[self.op](self.rhs.eval())
+    def eval(self, context):
+        return UniaryOp.op_map[self.op](self.rhs.eval(context))
 
     @classmethod
     def parse_action(cls, toks):
@@ -57,17 +74,20 @@ class ArithOp(BinaryOp):
         '-': lambda lhs, rhs: lhs - rhs,
     }
 
-    def eval(self):
-        return ArithOp.op_map[self.op](self.lhs.eval(), self.rhs.eval())
+    def eval(self, context):
+        return ArithOp.op_map[self.op](self.lhs.eval(context), self.rhs.eval(context))
 
 
 expr = pp.Forward()
+
 func_call = func_name + sym.LPARAM + \
-    pp.Optional(wildcard_all | pp.delimited_list(expr)) + sym.RPARAM
+    pp.Optional(pp.Group(wildcard_all | pp.delimited_list(expr))) + sym.RPARAM
+func_call.set_parse_action(FuncCall.parse_action)
+
 expr_term = (
-    literal_value
+    func_call
+    | literal_value
     | col_name
-    | func_call
 )
 
 UNARY, BINARY, TERTIARY = 1, 2, 3
@@ -91,7 +111,10 @@ expr << pp.infix_notation(
     [
         (pp.one_of('+ - ~') | kw.NOT, UNARY,
          pp.opAssoc.RIGHT, UniaryOp.parse_action),
-        (pp.one_of('/ % *') | kw.NOT, BINARY, pp.opAssoc.LEFT, ArithOp.parse_action),
+
+        (pp.one_of('/ % *') | kw.NOT, BINARY,
+         pp.opAssoc.LEFT, ArithOp.parse_action),
+
         (pp.one_of('+ -') | kw.NOT, BINARY, pp.opAssoc.LEFT, ArithOp.parse_action),
     ]
 )
@@ -106,6 +129,7 @@ if __name__ == '__main__':
     # res = expr.parse_string("1 + 4 * 5")[0]
     # print(res, res.eval())
 
-    exp = "(1 + 4) * 5 / 3 - 7 % 3"
-    res = expr.parse_string(exp)[0]
-    print(f'{exp}\n\n{res}\n\nresult: {res.eval()}')
+    exp = "(1 + sum(x, y)) * 5 / 3 - 7 % 3"
+    res = expr.parse_string(exp)
+    # print(res)
+    print(f'{exp}\n\n{res}\n\nresult: {res[0].eval({})}')
